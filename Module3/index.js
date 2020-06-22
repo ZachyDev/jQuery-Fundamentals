19f5c6d06749de026839992c6677f6f6f9be6415
ZachyDev <mosetizachary001@gmail.com>
2020-06-22 20:48:08 +0300

    Applying custom css styles

--- Module3/index.js
+++ Module3/index.js
@@ -3,9 +3,22 @@ $(document).ready(function(){
     // iterating through nodes
       var html = '';
     $('div.about,div.services').each(function(index){
+        // raw DOM object
         this.title = 'Some title';
+        // using jquery built in  functions
+        $(this).attr('title','Some title2');
        html += "<br />" + index + " " + $(this).text();
     })
     var output = $('div.output');
     output.html(html);
+
+    // changing the styles using att property in jquery
+    $('div.about').attr({
+        title: 'About section',
+        style:'background-color:teal;font-size:20px;margin-left:25%;height:20vh;color:#fff;width:50%;'
+    });
+    $('div.services').attr({
+        title: 'Services page',
+        style:'background-color:teal;font-size:20px;margin-left:25%;height:20vh;color:#fff;width:50%;'
+    })
 })

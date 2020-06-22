9823bfc94df7839c8b7f66598210bc578c0d0bd0
ZachyDev <mosetizachary001@gmail.com>
2020-06-22 18:39:29 +0300

    Module 3: Accessing and iterating through DOM elements in JavaScript

--- Module3/index.js
+++ Module3/index.js
@@ -1,3 +1,10 @@
-$(document).ready(function() {
-   
+$(document).ready(function(){
+    $('#header h1').css('color','blue');
+    // iterating through nodes
+      var html = '';
+    $('div.about,div.services').each(function(index){
+       html += "<br />" + index + " " + $(this).text();
+    })
+    var output = $('div.output');
+    output.html(html);
 })

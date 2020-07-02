3bd7a1db0a34d9d34645be1a2df35de2d24adbc1
ZachyDev <mosetizachary001@gmail.com>
2020-07-02 23:22:42 +0300

    handling errors while fetching data using load()

--- Module5/main.js
+++ Module5/main.js
@@ -9,6 +9,12 @@ function loadedData() {
     let btn = $('#load');
     let dataDiv = $('div.loaded-data');
     btn.on('click',function(){
-        dataDiv.load('../index.html .about');
+        dataDiv.load('../idex.html .about',
+        function(response,status,xhr) {
+            if (status == 'error') {
+                alert('Error: ' + xhr.statusText)
+            }
+        }
+        );
     })
 }
